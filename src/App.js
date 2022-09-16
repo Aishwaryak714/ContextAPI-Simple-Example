e6ac9b1e584cb3af70a46034c13f395e18944edd
Kumtale <aishwarya-bhausaheb.kumtale@capgemini.com>
2022-09-16 16:09:05 +0530

    Simple Example of ContextAPI in reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from 'react';
+import Child from './Child';
+import SuperChild from './SuperChild';
+import {CounterContext} from './CounterContext';
 
 function App() {
+
+  const [count,setCounter]=useState(0)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Context API Examples</h3>
+      <div style={{border:'1px solid',marginLeft:'480px',marginRight:'480px'}}>
+      <div>{count}</div>
+      <br/>
+      <div className='container'>
+      <div>
+        <button onClick={()=>setCounter(count+1)}>Increment</button>
+      </div>
+      <div>
+        <button onClick={()=>setCounter(count-1)}>Decrement</button>
+      </div>
+      <br/><br/>
+      </div>
+      </div>
+      <br/>
+      <CounterContext.Provider value={{count,setCounter}} >
+      <Child />
+      <SuperChild />
+      </CounterContext.Provider>
     </div>
   );
 }
---
+++ src/SuperChild.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useContext } from 'react'
+import {CounterContext} from './CounterContext'
+
+function SuperChild() {
+
+    const {count,setCounter}=useContext(CounterContext);
+
+  return (
+    <div>
+    <h3>SuperChild Component</h3>
+    <div style={{border:'1px solid',marginLeft:'480px',marginRight:'480px'}}>
+    <div>{count}</div>
+    <br/>
+    <div className='container'>
+    {/* <div>
+    <button onClick={()=>setCounter(count+1)}>Increment</button>
+    </div> */}
+    <div>
+      <button onClick={()=>setCounter(count-1)}>Decrement</button>
+    </div>
+    <br/><br/>
+    </div>
+    </div>
+  </div>
+  )
+}
+
+export default SuperChild
